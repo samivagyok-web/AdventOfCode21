5e8fd784d54893838fdb616759b22869a184d002
samivagyok-web <66529115+samivagyok-web@users.noreply.github.com>
2021-12-02 17:22:08 +0200

    Add files via upload

---
+++ Day2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SecondStar();
+        }
+
+        public static void SecondStar()
+        {
+            TextReader file = new StreamReader(@"2.in");
+            int forward = 0;
+            int depth = 0;
+            int aim = 0;
+
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                var coord = line.Split(" ");
+
+                if (coord[0].Equals("forward"))
+                {
+                    forward += int.Parse(coord[1]);
+                    depth += aim * int.Parse(coord[1]);
+                }
+                else if (coord[0].Equals("up"))
+                {
+                    aim -= int.Parse(coord[1]);
+                }
+                else
+                {
+                    aim += int.Parse(coord[1]);
+                }
+            }
+
+            Console.WriteLine(forward * depth);
+        }
+
+        public static void FirstStar()
+        {
+            TextReader file = new StreamReader(@"2.in");
+            int forward = 0;
+            int depth = 0;
+
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                var coord = line.Split(" ");
+
+                if (coord[0].Equals("forward"))
+                {
+                    forward += int.Parse(coord[1]);
+                }
+                else if (coord[0].Equals("up"))
+                {
+                    depth -= int.Parse(coord[1]);
+                }
+                else
+                {
+                    depth += int.Parse(coord[1]);
+                }
+            }
+        }
+    }
+}
